af7da23b8a4e2c90d403601e614c367dd5ea9dfb
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2022-08-10 23:10:15 +0530

    added feature

--- bot2.py
+++ bot2.py
@@ -91,6 +91,11 @@ def create(update: Update,context: CallbackContext):
 def validate(update,context,user_input=True,acess_key=True):
     key=update.message.text.reply_photo(photo=open(user_input,acess_key))
     return key
+def maru(update: Update,context: CallbackContext):
+    update.message.reply_text("""
+        Do you want to create a website""")
+
+
 
 def add(update: Update, context: CallbackContext):
     update.message.reply_text("Enter the image name  to view file")
